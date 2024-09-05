ba1a1d388fa1bb91c68e72ffdcb5dc4eb886c36e
MakashovAV <mak2440@yandex.ru>
2024-09-05 17:21:52 +0700

    # Дополнительное практическое задание по модулю: "Наследование классов."

--- module6hard.py
+++ module6hard.py
@@ -37,7 +37,7 @@ class Figure:
         if self.__is_valid_sides(*new_sides):
             self.__sides = [*new_sides]
         else:
-            print('Количество сторон не верно')
+            print('Количество сторон для данной фигуры не верно')
 
 
 class Circle(Figure):
@@ -96,19 +96,20 @@ class Cube(Figure):
         return self.get_sides()[0] ** 3
 
 
-circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
+circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
 cube1 = Cube((222, 35, 130), 6)
 
+
 # Проверка на изменение цветов:
-circle1.set_color(55, 66, 77)  # Изменится
+circle1.set_color(55, 66, 77) # Изменится
 print(circle1.get_color())
-cube1.set_color(300, 70, 15)  # Не изменится
+cube1.set_color(300, 70, 15) # Не изменится
 print(cube1.get_color())
 
 # Проверка на изменение сторон:
-cube1.set_sides(5, 3, 12, 4, 5)  # Не изменится
+cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
 print(cube1.get_sides())
-circle1.set_sides(15)  # Изменится
+circle1.set_sides(15) # Изменится
 print(circle1.get_sides())
 
 # Проверка периметра (круга), это и есть длина:
@@ -116,5 +117,10 @@ print(len(circle1))
 
 # Проверка объёма (куба):
 print(cube1.get_volume())
-print(len(cube1))
+
+# Проверка возможности треугольника
+triangle_try = Triangle((200, 200, 100), 5, 4, 2)
+triangle_false = Triangle((200, 200, 100), 100, 4, 2)
+
+
 
